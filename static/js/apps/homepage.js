b6d69ab9c94155dc7ad46d87f2efbc8be88db815
Adam Lord <adam.lord@gmail.com>
2013-09-25 18:18:52 -0700

    Homepage in a pretty good place.
    Signed-off-by: Adam Lord <adam.lord@gmail.com>

--- static/js/apps/homepage.js
+++ static/js/apps/homepage.js
@@ -1,22 +1,38 @@
 define([
     'jquery',
-
+    'underscore',
     'bootstrap',
 ],
 
 function ($) {
+    var w = $(window);
+    var nvo = 0;
+    function setHeights(){
+        var v = $('#video_wrap iframe');
+        var vh = Math.floor(0.5625 * w.width());
+        var ch = $('#bio .carousel').height();
+        var bt = $('#bio section');
+        var bth = bt.height();
+        v.css('min-height', vh + 'px');
+        bt.css({
+            'height': ch + 'px',
+            'padding-top': (ch - bth) / 2 + 'px'
+        });
+        nvo = $('#home_nav').offset().top;
+    }
+    function setFixed(){
+        $('#next_show').toggleClass('up', $(this).scrollTop() > (nvo - 40));
+        $('#home_nav, #nav_push').toggleClass('up', $(this).scrollTop() > nvo);
+    }
     $(function() {
         // Hompage specific js goes here, if not already in a module
-        $('#hero').carousel({
+        $('.carousel').carousel({
             pause: false
-        })
-        console.log('hello world');
-        var w = $(window);
-		var tb = $('#topbrand');
-		var tbh = tb.height();
-		w.scroll(function(){
-			$(this).scrollTop() > (pso - psh + w.height()) ? ps.addClass('in') : ps.removeClass('in');
-			$(this).scrollTop() > (tbh) ? tb.addClass('af') : tb.removeClass('af');
-		});
+        });
+        setHeights();
+        var resetHeights = _.debounce(setHeights, 300);
+        var resetFixed = _.throttle(setFixed, 100);
+        $(w).resize(resetHeights);
+        $(w).scroll(resetFixed);
     });
 });

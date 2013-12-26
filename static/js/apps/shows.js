d56a305baf80671d083b2940f45071615573ef17
Adam Lord <adam.lord@gmail.com>
2013-12-26 15:25:45 -0800

    map troubleshooting
    Signed-off-by: Adam Lord <adam.lord@gmail.com>

--- static/js/apps/shows.js
+++ static/js/apps/shows.js
@@ -5,6 +5,7 @@ define([
 
 function ($) {
 	function initMap(ltlng) {
+		console.log('initmap')
 		var thisltlng = new google.maps.LatLng(ltlng.split(",")[0], ltlng.split(",")[1]);
 		var mapOptions = {
 			zoom: 12,
@@ -12,10 +13,14 @@ function ($) {
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+		console.log('map')
+		console.log(map)
 		var marker = new google.maps.Marker({
 			position: thisltlng,
 			map: map,
 		});
+		console.log('marker')
+		console.log(marker)
 		return map;
 	}
 	$('.detail-launch').click(function(e){
@@ -28,8 +33,13 @@ function ($) {
         });
 	});
 	$('#show_detail_modal').on('shown.bs.modal', function(){
+		console.log('show modal')
 		var ltlng = $(this).find('#venue_address').val();
+		console.log(ltlng)
 		var modalMap = initMap(ltlng);
+		console.log('modalMap')
+		console.log(modalMap)
 		google.maps.event.trigger(modalMap, "resize");
+		console.log('reszied')
 	});
 });

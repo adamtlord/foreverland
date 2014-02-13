08e5384dc4eed2fe4ea9ffdad15379fa8ee3ff97
Adam Lord <adam.lord@gmail.com>
2014-02-12 17:55:50 -0800

    Add more fidouche features
    Signed-off-by: Adam Lord <adam.lord@gmail.com>

--- apps/fidouche/views.py
+++ apps/fidouche/views.py
@@ -11,43 +11,42 @@ current_year = date.today().year
 
 @login_required
 def financial_dashboard(request, template='fidouche/dashboard.html'):
-    """"""
-    gigs = Show.objects.all().order_by('-date')
-    last_show = gigs.filter(date__lte=datetime.datetime.now()).order_by('-date')[0]
-    next_show = gigs.filter(date__gte=datetime.datetime.now()).order_by('date')[0]
-    gigs_booked = gigs.filter(date__year=current_year)
-    gigs_played = gigs_booked.filter(date__lt=datetime.datetime.now())
-    ytd_gross = []
-    ytd_net = []
-    ytd_player = []
-    for gig in gigs_played:
-    	ytd_gross.append(gig.gross)
-    	ytd_net.append(gig.net)
-    	ytd_player.append(gig.payout)
+	""""""
+	gigs = Show.objects.all().order_by('-date')
+	last_show = gigs.filter(date__lte=datetime.datetime.now()).order_by('-date')[0]
+	next_show = gigs.filter(date__gte=datetime.datetime.now()).order_by('date')[0]
+	gigs_booked = gigs.filter(date__year=current_year)
+	gigs_played = gigs_booked.filter(date__lt=datetime.datetime.now())
+	ytd_gross = []
+	ytd_net = []
+	ytd_player = []
+	for gig in gigs_played:
+		ytd_gross.append(gig.gross)
+		ytd_net.append(gig.net)
+		ytd_player.append(gig.payout)
 
-    ytd = {
-    	'gigs_booked': gigs_booked.count(),
-    	'gigs_played': gigs_played.count(),
-    	'net':	sum(ytd_net),
-    	'gross': sum(ytd_gross),
-    	'payout': sum(ytd_player)
-    }
-    d = {
-    	'gigs': gigs,
-    	'last_show': last_show,
-    	'next_show': next_show,
-    	'ytd': ytd
-    }
-    return render(request, template, d)
+	ytd = {
+		'gigs_booked': gigs_booked.count(),
+		'gigs_played': gigs_played.count(),
+		'net':	sum(ytd_net),
+		'gross': sum(ytd_gross),
+		'payout': sum(ytd_player)
+	}
+	d = {
+		'gigs': gigs,
+		'last_show': last_show,
+		'next_show': next_show,
+		'ytd': ytd
+	}
+	return render(request, template, d)
 
 @login_required
 def gigs_by_year(request, year=current_year, template='fidouche/gigs_by_year.html'):
 	"""Choose a gig from this year"""
 	gigs = Show.objects.filter(date__year = year)
-
 	d = {
 		'year': year,
-		'this_years_gigs': gigs
+		'this_years_gigs': gigs,
 	}
 	return render(request, template, d)
 
